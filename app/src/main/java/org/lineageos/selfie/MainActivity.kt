e2cbb2d8caab453e2df72f4e512c1fdceba99703
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-07 22:22:56 +0200

    Selfie: Improve vendor extensions handling
    * Make getSupportedModes a ExtensionsManager method
    * Always set supportedExtensionModes, we can query them regardless of the camera mode
    * Restore extensionModeIndex as well (fixes wrong icon on app reopening)
    
    Change-Id: I6ffca1c56f4ab23b5c2cb8f31d1e5d82bb1f9c3b

--- app/src/main/java/org/lineageos/selfie/MainActivity.kt
+++ app/src/main/java/org/lineageos/selfie/MainActivity.kt
@@ -318,8 +318,14 @@ class MainActivity : AppCompatActivity() {
             else -> CameraSelector.DEFAULT_BACK_CAMERA
         }
 
+        // Get the supported vendor extensions for the given camera selector
+        supportedExtensionModes = extensionsManager.getSupportedModes(cameraSelector)
+
         // Get the user selected effect
         extensionMode = sharedPreferences.getPhotoEffect()
+        if (!supportedExtensionModes.contains(extensionMode))
+            extensionMode = ExtensionMode.NONE
+        extensionModeIndex = supportedExtensionModes.indexOf(extensionMode)
 
         // Initialize the use case we want
         cameraMode = sharedPreferences.getLastCameraMode()
@@ -330,9 +336,6 @@ class MainActivity : AppCompatActivity() {
 
         // Only photo mode supports vendor extensions for now
         if (cameraMode == CameraMode.PHOTO) {
-            // Fetch the supported extensions
-            supportedExtensionModes =
-                ExtensionModeExt.getSupportedModes(extensionsManager, cameraSelector)
             // Select the extension
             if (supportedExtensionModes.contains(extensionMode)) {
                 cameraSelector = extensionsManager.getExtensionEnabledCameraSelector(
