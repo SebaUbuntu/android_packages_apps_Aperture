adc86119651b3c555d22ef4209bccfcf5d8a8675
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-07 15:08:22 +0200

    Aperture: Go back to using PhysicalCamera for camera facing
    * CameraSelector might not be DEFAULT_FRONT_CAMERA due to vendor extensions, thus always defaulting to back camera
    
    Change-Id: I14e568172d9e2b6ad6bf11c862aa02ee1ac69b40

--- app/src/main/java/org/lineageos/aperture/CameraControllerExt.kt
+++ app/src/main/java/org/lineageos/aperture/CameraControllerExt.kt
@@ -1,13 +1,9 @@
 package org.lineageos.aperture
 
-import androidx.camera.core.CameraSelector
 import androidx.camera.view.CameraController
-import org.lineageos.aperture.utils.CameraFacing
+import org.lineageos.aperture.utils.PhysicalCamera
 
-internal fun CameraController.cameraFacing(): CameraFacing {
-    return if (cameraSelector == CameraSelector.DEFAULT_FRONT_CAMERA) {
-        CameraFacing.FRONT
-    } else {
-        CameraFacing.BACK
-    }
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+internal fun CameraController.physicalCamera(): PhysicalCamera? {
+    return cameraInfo?.let { PhysicalCamera(it) }
 }
