2f9f81d8cc5724ebc2187e357a386454b51d912d
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-08-07 21:56:57 +0200

    Aperture: Iterate only over support cam extensions
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I23aea618e64b24928cbca74b4611ec8215cf2a13

---
+++ app/src/main/java/org/lineageos/aperture/utils/ExtensionModeExt.kt
@@ -0,0 +1,18 @@
+package org.lineageos.aperture.utils
+
+import androidx.camera.core.CameraSelector
+import androidx.camera.extensions.ExtensionMode
+import androidx.camera.extensions.ExtensionsManager
+
+object ExtensionModeExt {
+    private val extensionModes =
+        IntProgression.fromClosedRange(ExtensionMode.NONE, ExtensionMode.AUTO, 1)
+
+    fun getSupportedModes(
+        extensionsManager: ExtensionsManager,
+        cameraSelector: CameraSelector
+    ) = extensionModes.filter {
+        extensionsManager.isExtensionAvailable(cameraSelector, it)
+    }
+}
+
