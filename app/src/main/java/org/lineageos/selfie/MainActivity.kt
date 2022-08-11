9a620cd14eab4fe62905e8c825fa1c08c8db8d25
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-08-11 17:58:41 +0200

    Selfie: Don't use library only functions
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I0a525145786b0b76c857a81033ef69e7fb0c88b5

--- app/src/main/java/org/lineageos/selfie/MainActivity.kt
+++ app/src/main/java/org/lineageos/selfie/MainActivity.kt
@@ -46,6 +46,7 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.camera.view.video.Metadata
 import androidx.camera.view.video.OnVideoSavedCallback
 import androidx.camera.view.video.OutputFileResults
 import androidx.core.app.ActivityCompat
@@ -372,8 +373,12 @@ class MainActivity : AppCompatActivity() {
         shutterButton.isEnabled = false
 
         // Create output options object which contains file + metadata
-        val outputOptions = StorageUtils.getPhotoMediaStoreOutputOptions(contentResolver)
-        outputOptions.metadata.location = location
+        val outputOptions = StorageUtils.getPhotoMediaStoreOutputOptions(
+            contentResolver,
+            ImageCapture.Metadata().apply {
+                location = this@MainActivity.location
+            }
+        )
 
         // Set up image capture listener, which is triggered after photo has
         // been taken
@@ -416,7 +421,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Create output options object which contains file + metadata
-        val outputOptions = StorageUtils.getVideoMediaStoreOutputOptions(contentResolver)
+        val outputOptions = StorageUtils.getVideoMediaStoreOutputOptions(
+            contentResolver,
+            Metadata.builder().apply {
+                setLocation(location)
+            }.build()
+        )
 
         // Play shutter sound
         if (cameraSoundsUtils.playStartVideoRecording()) {
