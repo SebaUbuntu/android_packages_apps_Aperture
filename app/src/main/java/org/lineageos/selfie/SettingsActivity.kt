e6b53c1046d2fc2a7969e41a9e27d6dfee16f4c4
LuK1337 <priv.luk@gmail.com>
2022-08-13 10:35:44 +0200

    Selfie: Move location permission handling to SettingsActivity
    Change-Id: I77b45992f3aa192dbbfa9da66fb6d745ad84ff23

--- app/src/main/java/org/lineageos/selfie/SettingsActivity.kt
+++ app/src/main/java/org/lineageos/selfie/SettingsActivity.kt
@@ -6,9 +6,14 @@
 
 package org.lineageos.selfie
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import org.lineageos.selfie.utils.CameraSoundsUtils
@@ -38,11 +43,43 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+        private val saveLocation by lazy { findPreference<SwitchPreference>("save_location") }
         private val shutterSound by lazy { findPreference<SwitchPreference>("shutter_sound") }
+
+        private val requestLocationPermissions = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            if (it.any { permission -> !permission.value }) {
+                saveLocation?.isChecked = false
+                Toast.makeText(
+                    requireContext(), getString(R.string.save_location_toast), Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            saveLocation?.let {
+                // Reset location back to off if permissions aren't granted
+                it.isChecked = it.isChecked && allLocationPermissionsGranted()
+                it.onPreferenceChangeListener =
+                    Preference.OnPreferenceChangeListener { _, newValue ->
+                        if (newValue as Boolean) {
+                            requestLocationPermissions.launch(
+                                MainActivity.REQUIRED_PERMISSIONS_LOCATION
+                            )
+                        }
+                        true
+                    }
+            }
             shutterSound?.isEnabled = !CameraSoundsUtils.mustPlaySounds
         }
+
+        private fun allLocationPermissionsGranted() =
+            MainActivity.REQUIRED_PERMISSIONS_LOCATION.all {
+                ContextCompat.checkSelfPermission(
+                    requireContext(), it
+                ) == PackageManager.PERMISSION_GRANTED
+            }
     }
 }
