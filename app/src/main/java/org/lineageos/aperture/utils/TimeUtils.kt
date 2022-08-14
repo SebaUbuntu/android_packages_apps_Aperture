ddfcb3fa9b56be24b29b974964a418797b6038ff
LuK1337 <priv.luk@gmail.com>
2022-08-15 00:28:36 +0200

    Aperture: nanos -> seconds
    Change-Id: I9379962fdab1ce1f08b804293ea4c2e55621a21f

--- app/src/main/java/org/lineageos/aperture/utils/TimeUtils.kt
+++ app/src/main/java/org/lineageos/aperture/utils/TimeUtils.kt
@@ -10,12 +10,12 @@ import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 object TimeUtils {
-    fun convertSecondsToString(nanos: Long): String {
+    fun convertSecondsToString(seconds: Long): String {
         return java.lang.String.format(
             Locale.US, "%02d:%02d:%02d",
-            TimeUnit.SECONDS.toHours(nanos),
-            TimeUnit.SECONDS.toMinutes(nanos) % TimeUnit.HOURS.toMinutes(1),
-            TimeUnit.SECONDS.toSeconds(nanos) % TimeUnit.MINUTES.toSeconds(1)
+            TimeUnit.SECONDS.toHours(seconds),
+            TimeUnit.SECONDS.toMinutes(seconds) % TimeUnit.HOURS.toMinutes(1),
+            TimeUnit.SECONDS.toSeconds(seconds) % TimeUnit.MINUTES.toSeconds(1)
         )
     }
 }
