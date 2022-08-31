efd8fda0eb1ae4cf34b84d0a99b563e13bdead39
LuK1337 <priv.luk@gmail.com>
2022-08-31 21:54:41 +0200

    Aperture: Introduce CameraState enum
    This lets us keep track of the state of camera without using multiple
    variables.
    
    Change-Id: Idee89d4caad8653c0a14e0f33d22aa1fd81817cc

---
+++ app/src/main/java/org/lineageos/aperture/utils/CameraState.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.aperture.utils
+
+enum class CameraState {
+    IDLE,
+    TAKING_PHOTO,
+    PRE_RECORDING_VIDEO,
+    RECORDING_VIDEO,
+    RECORDING_VIDEO_PAUSED,
+}
