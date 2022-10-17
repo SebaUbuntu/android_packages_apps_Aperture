5e225d9c3c49c56c50694c89ec1df2e701e0300a
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-10-17 20:56:47 +0200

    Aperture: Use gradient background for exposure slider
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: If15e9bbcd68c2cdd2fe77a797444271082c398c8

--- app/src/main/java/org/lineageos/aperture/ui/Slider.kt
+++ app/src/main/java/org/lineageos/aperture/ui/Slider.kt
@@ -8,11 +8,13 @@ package org.lineageos.aperture.ui
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.LinearGradient
 import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import android.graphics.Rect
 import android.graphics.RectF
+import android.graphics.Shader
 import android.util.AttributeSet
 import android.view.View
 import org.lineageos.aperture.R
@@ -28,6 +30,12 @@ abstract class Slider @JvmOverloads constructor(
         setShadowLayer(1f, 0f, 0f, Color.BLACK)
     }
 
+    private val trackBorderPaint = Paint().apply {
+        style = Paint.Style.STROKE
+        xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC)
+        strokeWidth = 2F
+    }
+
     private val thumbPaint = Paint().apply {
         style = Paint.Style.FILL
         xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC)
@@ -35,6 +43,8 @@ abstract class Slider @JvmOverloads constructor(
     }
 
     private val thumbTextPaint = Paint()
+
+    private var gradientColors: IntArray
 
     var progress = 0.5f
         set(value) {
@@ -58,8 +68,26 @@ abstract class Slider @JvmOverloads constructor(
     init {
         context.obtainStyledAttributes(attrs, R.styleable.Slider, 0, 0).apply {
             try {
-                trackPaint.color = getColor(R.styleable.Slider_trackColor, Color.WHITE)
+                if (getBoolean(R.styleable.Slider_trackColorGradient, false)) {
+                    val trackColorGradientStart =
+                        getColor(R.styleable.Slider_trackColorGradientStart, Color.WHITE)
+                    val trackColorGradientCenter =
+                        getColor(R.styleable.Slider_trackColorGradientCenter, Color.WHITE)
+                    val trackColorGradientEnd =
+                        getColor(R.styleable.Slider_trackColorGradientEnd, Color.WHITE)
+                    gradientColors = intArrayOf(
+                        trackColorGradientStart,
+                        trackColorGradientCenter,
+                        trackColorGradientEnd
+                    )
+                } else {
+                    gradientColors = intArrayOf()
+                    trackPaint.color = getColor(R.styleable.Slider_trackColor, Color.WHITE)
+                }
                 thumbPaint.color = getColor(R.styleable.Slider_thumbColor, Color.BLACK)
+                trackBorderPaint.color = getColor(
+                    R.styleable.Slider_trackBorderColor, Color.TRANSPARENT
+                )
                 thumbTextPaint.color = getColor(R.styleable.Slider_thumbTextColor, Color.WHITE)
                 thumbTextPaint.textSize =
                     getDimension(R.styleable.Slider_thumbTextSize, 10.px.toFloat())
@@ -84,8 +112,23 @@ abstract class Slider @JvmOverloads constructor(
         val track = track()
         val trackRadius = track.width() * 0.75f
 
+        if (gradientColors.isNotEmpty()) {
+            trackPaint.shader = LinearGradient(
+                track().width() / 2,
+                0f,
+                track().width() / 2,
+                track.height(),
+                gradientColors,
+                null,
+                Shader.TileMode.CLAMP
+            )
+        }
+
         // Draw round rect
         canvas.drawRoundRect(track, trackRadius, trackRadius, trackPaint)
+
+        // Draw border
+        canvas.drawRoundRect(track, trackRadius, trackRadius, trackBorderPaint)
     }
 
     abstract fun thumb(): Triple<Float, Float, Float>
