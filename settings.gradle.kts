1c14caa59b87376a47abe8c2ea7c8c72a75edb6d
LuK1337 <priv.luk@gmail.com>
2022-10-18 00:05:12 +0200

    Aperture: Add option for unignoring non-backward compatible camera IDs
    Change-Id: I244d2097278b8343bc0a2fd1b33df6e4fcbf4cda

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://raw.githubusercontent.com/luk1337/camerax_selfie/6b2510d11a28eb5e32a0e17fea3f91bc37ae4316/.m2")
+        maven("https://raw.githubusercontent.com/luk1337/camerax_selfie/84db72d746f38fbf4def600aac6d730c4cb3dae6/.m2")
         google()
         mavenCentral()
     }
