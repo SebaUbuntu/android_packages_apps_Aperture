5a784bbce93e2900139918470262a1ff23bfcfb0
LuK1337 <priv.luk@gmail.com>
2022-12-01 19:01:02 +0100

    Aperture: Update com.google.android.material to 1.8.0-alpha03
    Change-Id: I377ab0469ad09e4a2167ddf7d9755413b2d985fa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("androidx.preference:preference:1.2.0")
-    implementation("com.google.android.material:material:1.8.0-alpha02")
+    implementation("com.google.android.material:material:1.8.0-alpha03")
 
     // CameraX core library using the camera2 implementation
     val cameraxVersion = "1.2.0-rc02"
