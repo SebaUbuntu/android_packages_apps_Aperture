5bae4e780f1709334c29a53a73533ca2131f245e
LuK1337 <priv.luk@gmail.com>
2022-12-11 23:47:17 +0100

    Revert "Aperture: Import SharedPreferences.edit from AndroidX"
    AndroidX on lineage-20 is new enough to contain it.
    
    This reverts commit 0b36ed100610bd3708bac095c85a5a60d28b1301.
    
    Change-Id: I7d1ec6550a9d9834801a88b5debc1aa10b7f8d03

--- app/src/main/java/org/lineageos/aperture/SharedPreferencesExt.kt
+++ app/src/main/java/org/lineageos/aperture/SharedPreferencesExt.kt
@@ -5,33 +5,19 @@
 
 package org.lineageos.aperture
 
-import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.net.Uri
 import androidx.camera.core.AspectRatio
 import androidx.camera.core.ImageCapture
 import androidx.camera.extensions.ExtensionMode
 import androidx.camera.video.Quality
+import androidx.core.content.edit
 import org.lineageos.aperture.utils.CameraFacing
 import org.lineageos.aperture.utils.CameraMode
 import org.lineageos.aperture.utils.FlashMode
 import org.lineageos.aperture.utils.Framerate
 import org.lineageos.aperture.utils.GridMode
 import org.lineageos.aperture.utils.StabilizationMode
-
-@SuppressLint("ApplySharedPref")
-inline fun SharedPreferences.edit(
-    commit: Boolean = false,
-    action: SharedPreferences.Editor.() -> Unit
-) {
-    val editor = edit()
-    action(editor)
-    if (commit) {
-        editor.commit()
-    } else {
-        editor.apply()
-    }
-}
 
 // Generic prefs
 private const val LAST_CAMERA_FACING_KEY = "last_camera_facing"
