3004f3595125ced48c46dbe48bfecd5ad0e5ae15
LuK1337 <priv.luk@gmail.com>
2022-12-20 10:43:54 +0100

    Aperture: Update CameraX to 1.3.0-alpha03
    Change-Id: I214254ba8027b099e02481ec7aedf8e7c11d864f

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://raw.githubusercontent.com/luk1337/camerax_selfie/4532191e32b1d2c59753f5985878e245875d5df7/.m2")
+        maven("https://raw.githubusercontent.com/luk1337/camerax_selfie/4ec143fff3b6918fe1d9cf3787fc8424cb1b32b9/.m2")
         google()
         mavenCentral()
     }
