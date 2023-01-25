a0548a12fda711bd72d957bfea15d871d18fde8e
LuK1337 <priv.luk@gmail.com>
2023-01-25 19:51:14 +0100

    Aperture: Update dependencies
    Change-Id: I6abc89d24162db28e5236b2112ea6f139e9a8c24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,11 +52,11 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("androidx.preference:preference:1.2.0")
-    implementation("com.google.android.material:material:1.8.0-beta01")
+    implementation("com.google.android.material:material:1.9.0-alpha01")
 
     // CameraX core library using the camera2 implementation
     val cameraxVersion = "1.2.0"
