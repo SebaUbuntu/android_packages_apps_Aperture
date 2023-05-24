b2d505b38dbec6e82cfa83c3239d62baf1c79664
LuK1337 <priv.luk@gmail.com>
2023-05-24 20:25:10 +0200

    Aperture: Update CameraX to 1.3.0-alpha07
    Change-Id: Icc2582e79728097ebe73b501d7836011b333782b

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0-alpha02")
 
     // CameraX core library using the camera2 implementation
-    val cameraxVersion = "1.3.0-alpha06"
+    val cameraxVersion = "1.3.0-alpha07"
     // The following line is optional, as the core library is included indirectly by camera-camera2
     implementation("androidx.camera:camera-core:${cameraxVersion}")
     implementation("androidx.camera:camera-camera2:${cameraxVersion}")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://raw.githubusercontent.com/luk1337/camerax_selfie/8fce720404a9b396a6f38a0be598a3e63002b2f2/.m2")
+        maven("https://raw.githubusercontent.com/luk1337/camerax_selfie/1306d8431684a0225f41986501833772f3c28ba4/.m2")
         google()
         mavenCentral()
     }
