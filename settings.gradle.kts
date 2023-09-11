1bd366252470aec851c70bc403c5dbefcb705a68
LuK1337 <priv.luk@gmail.com>
2023-09-11 20:23:13 +0200

    Aperture: Update CameraX maven repo URL
    Change-Id: I54f301df9ed64239db94037bc4f309814f957921

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://raw.githubusercontent.com/luk1337/camerax_selfie/fb5d75481f9639eeeab1a4f7b26d23533b9be04e/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/camerax-aperture/fb5d75481f9639eeeab1a4f7b26d23533b9be04e/.m2")
         google()
         mavenCentral()
     }
