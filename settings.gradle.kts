3006e8cdf2bba429ba64cef8ff3253643e1647ad
LuK1337 <priv.luk@gmail.com>
2023-10-18 20:32:10 +0200

    Aperture: Update CameraX to 1.4.0-alpha02
    Change-Id: I77edb758b2798e80045e7dce02a73a636eb66665

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://raw.githubusercontent.com/lineage-next/camerax-aperture/54901603849cb157716f4ff65bb85ab0538c30e7/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/camerax-aperture/2ac992438b7c5eb3783b79007d0dee2cd044e893/.m2")
         google()
         mavenCentral()
     }
